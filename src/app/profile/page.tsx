a4f8ee6f5cc734fcc51f7c007e63a41bb81b8584
Гриньков Владислав Леонидович <vladislav.grinkov@rt.ru>
2023-05-17 13:10:01 +0300

    update qr

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -154,7 +154,7 @@ export default function Second() {
                     Отсканируйте QR-код на телефоне и перейдите по ссылке!
                   </p>
                   <div className="bg-white w-1/3">
-                    <img height={800} width={800} src="/qr_code.jpg" />
+                    <img height={800} width={800} src="/new_qrCode.png" />
                   </div>
                 </div>
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
@@ -171,52 +171,6 @@ export default function Second() {
           </div>
           <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
         </>
-
-        // <div
-        //   aria-hidden="true"
-        //   className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-        // >
-        //   <div className="relative w-full max-w-2xl max-h-full">
-        //     <div className="relative rounded-lg shadow bg-fuchsia-400">
-        //       <div className="flex items-start justify-between p-4 border-b rounded-t border-white">
-        // <h3 className="text-xl text-white dark:text-white">
-        //   Хотите пообщаться ? <br />
-        // </h3>
-        // <button
-        //   type="button"
-        //   onClick={() => setOpen(false)}
-        //   className="text-white bg-transparent hover:text-fuchsia-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-fuchsia-600 dark:hover:text-white"
-        //   data-modal-hide="defaultModal"
-        // >
-        //   <svg
-        //     aria-hidden="true"
-        //     className="w-5 h-5"
-        //     fill="currentColor"
-        //     viewBox="0 0 20 20"
-        //     xmlns="http://www.w3.org/2000/svg"
-        //   >
-        //     <path
-        //       fillRule="evenodd"
-        //       d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-        //       clipRule="evenodd"
-        //     ></path>
-        //   </svg>
-        // </button>
-        //       </div>
-
-        //       <div className="p-6 space-y-6">
-        // <p className="text-white">
-        //   Отсканируйте QR-код на телефоне и перейдите по ссылке!
-        // </p>
-        //         <div className="flex items-center justify-center">
-        // <div className="bg-white w-1/3">
-        //   <img height={300} width={300} src="/qr_code.jpg" />
-        // </div>
-        //         </div>
-        //       </div>
-        //     </div>
-        //   </div>
-        // </div>
       )}
     </div>
   );
